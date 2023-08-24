a75d7fcd62eb939f2f9baabe94ae052745954980
Boyan Georgiev <boyan_georgiev88@abv.bg>
2023-08-25 00:53:01 +0300

    tests for parsing all possible values

---
+++ src/test/java/com/github/chaosfirebolt/converter/RomanIntegerParseAllValuesTests.java
@@ -0,0 +1,28 @@
+package com.github.chaosfirebolt.converter;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class RomanIntegerParseAllValuesTests {
+
+    private static final String ARABIC_ERROR_MESSAGE = "Arabic representation not as expected";
+    private static final String ROMAN_ERROR_MESSAGE = "Roman representation not as expected";
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/all-values.csv", numLinesToSkip = 1)
+    public void arabicToRoman(String arabic, String roman) {
+        RomanInteger result = RomanInteger.parse(arabic);
+        assertEquals(Integer.parseInt(arabic), result.getArabic(), ARABIC_ERROR_MESSAGE);
+        assertEquals(roman, result.toString(), ROMAN_ERROR_MESSAGE);
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/all-values.csv", numLinesToSkip = 1)
+    public void romanToArabic(int arabic, String roman) {
+        RomanInteger result = RomanInteger.parse(roman);
+        assertEquals(arabic, result.getArabic(), ARABIC_ERROR_MESSAGE);
+        assertEquals(roman, result.toString(), ROMAN_ERROR_MESSAGE);
+    }
+}
