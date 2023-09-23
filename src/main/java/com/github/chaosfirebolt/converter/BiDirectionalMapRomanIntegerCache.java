6d2c4b8ec9c1dd7e896351554707566954c1f2bd
Boyan Georgiev <boyan_georgiev88@abv.bg>
2023-09-23 16:33:07 +0300

    bidirectional map based roman integer cache
    - implmentation
    - unit tests

---
+++ src/main/java/com/github/chaosfirebolt/converter/BiDirectionalMapRomanIntegerCache.java
@@ -0,0 +1,32 @@
+package com.github.chaosfirebolt.converter;
+
+import com.github.chaosfirebolt.converter.api.cache.ParserCache;
+
+import java.util.Map;
+import java.util.function.Function;
+
+/**
+ * Caches result in both directions - arabic and roman representations are mapped to the same {@link RomanInteger} instance.
+ */
+public class BiDirectionalMapRomanIntegerCache extends MapRomanIntegerCache {
+
+    /**
+     * Constructs an instance of Map based bidirectional roman integer cache
+     * @param parserCache parsers cache
+     */
+    public BiDirectionalMapRomanIntegerCache(ParserCache parserCache) {
+        super(parserCache);
+    }
+
+    @Override
+    protected RomanInteger cacheIfAbsent(Map<String, RomanInteger> cache, String key, Function<String, RomanInteger> computation) {
+        RomanInteger cached = cache.get(key);
+        if (cached != null) {
+            return cached;
+        }
+        RomanInteger computed = computation.apply(key);
+        cache.putIfAbsent(computed.getRoman(), computed);
+        cache.putIfAbsent(Integer.toString(computed.getArabic()), computed);
+        return computed;
+    }
+}
