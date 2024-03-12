66662c0829cc98554099e98b868b8216f4913f68
Boyan Georgiev <boyan_georgiev88@abv.bg>
2024-03-12 19:02:37 +0200

    default roman integer cache implementation for customization

--- src/main/java/com/github/chaosfirebolt/converter/BiDirectionalRomanIntegerCache.java
+++ src/main/java/com/github/chaosfirebolt/converter/BiDirectionalRomanIntegerCache.java
@@ -1,17 +1,10 @@
 package com.github.chaosfirebolt.converter;
 
-import com.github.chaosfirebolt.converter.api.OperationFailure;
-import com.github.chaosfirebolt.converter.api.cache.DefaultCache;
 import com.github.chaosfirebolt.converter.api.cache.ParserCache;
-import com.github.chaosfirebolt.converter.api.cache.RomanIntegerCache;
 import com.github.chaosfirebolt.converter.api.cache.storage.Computation;
-import com.github.chaosfirebolt.converter.api.cache.storage.MapStorage;
 import com.github.chaosfirebolt.converter.api.cache.storage.Storage;
 import com.github.chaosfirebolt.converter.api.initialization.InitializationData;
-import com.github.chaosfirebolt.converter.api.initialization.RomanIntegerArrayInitializationData;
-import com.github.chaosfirebolt.converter.api.initialization.source.BasicNumeralsInputSource;
 
-import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 import java.util.function.Supplier;
@@ -21,13 +14,13 @@ import java.util.function.Supplier;
  *
  * @since 3.3.0
  */
-public final class BiDirectionalRomanIntegerCache extends DefaultCache<String, RomanInteger> implements RomanIntegerCache {
+public final class BiDirectionalRomanIntegerCache extends DefaultRomanIntegerCache {
 
   /**
    * @param parserCache cache for parser instances
    */
   public BiDirectionalRomanIntegerCache(ParserCache parserCache) {
-    this(parserCache, MapStorage.lenient(HashMap::new), new RomanIntegerArrayInitializationData(new BasicNumeralsInputSource()));
+    super(parserCache);
   }
 
   /**
@@ -36,7 +29,7 @@ public final class BiDirectionalRomanIntegerCache extends DefaultCache<String, R
    * @param initializationData data to pre initialize the cache with
    */
   public BiDirectionalRomanIntegerCache(ParserCache parserCache, Storage<String, RomanInteger> storage, InitializationData<Map<String, RomanInteger>> initializationData) {
-    super(storage, Computation.wrap(new ParseComputation(parserCache)), initializationData, () -> new OperationFailure("Caching failure"));
+    super(parserCache, storage, initializationData);
   }
 
   @Override
--- src/main/java/com/github/chaosfirebolt/converter/UniDirectionalRomanIntegerCache.java
+++ src/main/java/com/github/chaosfirebolt/converter/UniDirectionalRomanIntegerCache.java
@@ -1,17 +1,9 @@
 package com.github.chaosfirebolt.converter;
 
-import com.github.chaosfirebolt.converter.api.OperationFailure;
-import com.github.chaosfirebolt.converter.api.cache.DefaultCache;
 import com.github.chaosfirebolt.converter.api.cache.ParserCache;
-import com.github.chaosfirebolt.converter.api.cache.RomanIntegerCache;
-import com.github.chaosfirebolt.converter.api.cache.storage.Computation;
-import com.github.chaosfirebolt.converter.api.cache.storage.MapStorage;
 import com.github.chaosfirebolt.converter.api.cache.storage.Storage;
 import com.github.chaosfirebolt.converter.api.initialization.InitializationData;
-import com.github.chaosfirebolt.converter.api.initialization.RomanIntegerArrayInitializationData;
-import com.github.chaosfirebolt.converter.api.initialization.source.BasicNumeralsInputSource;
 
-import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -19,13 +11,13 @@ import java.util.Map;
  *
  * @since 3.3.0
  */
-public final class UniDirectionalRomanIntegerCache extends DefaultCache<String, RomanInteger> implements RomanIntegerCache {
+public final class UniDirectionalRomanIntegerCache extends DefaultRomanIntegerCache {
 
   /**
    * @param parserCache cache for parser instances
    */
   public UniDirectionalRomanIntegerCache(ParserCache parserCache) {
-    this(parserCache, MapStorage.lenient(HashMap::new), new RomanIntegerArrayInitializationData(new BasicNumeralsInputSource()));
+    super(parserCache);
   }
 
   /**
@@ -34,6 +26,6 @@ public final class UniDirectionalRomanIntegerCache extends DefaultCache<String, 
    * @param initializationData data to pre initialize the cache with
    */
   public UniDirectionalRomanIntegerCache(ParserCache parserCache, Storage<String, RomanInteger> storage, InitializationData<Map<String, RomanInteger>> initializationData) {
-    super(storage, Computation.wrap(new ParseComputation(parserCache)), initializationData, () -> new OperationFailure("Caching failure"));
+    super(parserCache, storage, initializationData);
   }
 }
