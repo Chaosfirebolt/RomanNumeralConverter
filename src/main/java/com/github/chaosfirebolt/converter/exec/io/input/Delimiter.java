fbc2eabc1fed3ca6ca5690f5bfc30ff92ed7508a
Boyan Georgiev <boyan_georgiev88@abv.bg>
2025-01-25 18:55:56 +0200

    reader, default reader impl, tests

---
+++ src/main/java/com/github/chaosfirebolt/converter/exec/io/input/Delimiter.java
@@ -0,0 +1,69 @@
+package com.github.chaosfirebolt.converter.exec.io.input;
+
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.regex.Pattern;
+
+/**
+ * Represents a delimiter used to split a string.
+ * An empty delimiter does not split the string.
+ */
+public class Delimiter implements Function<String, String[]> {
+
+  private final String value;
+
+  private Delimiter(String value) {
+    this.value = value;
+  }
+
+  /**
+   * Creates a delimiter. Invoking this method with empty (null or empty string) value creates an empty delimiter.
+   * See {@link #empty()}.
+   *
+   * @param value value of the delimiter
+   * @return a delimiter instance
+   */
+  public static Delimiter of(String value) {
+    if (isEmpty(value)) {
+      return empty();
+    }
+    return new Delimiter(value);
+  }
+
+  private static boolean isEmpty(String value) {
+    return value == null || value.isEmpty();
+  }
+
+  /**
+   * Creates an empty delimiter.
+   *
+   * @return an empty delimiter instance.
+   */
+  public static Delimiter empty() {
+    return new Delimiter(null);
+  }
+
+  private Optional<String> regexValue() {
+    if (isEmpty(value)) {
+      return Optional.empty();
+    }
+    return Optional.of(Pattern.quote(value));
+  }
+
+  /**
+   * Splits the line using this delimiter literal value.
+   *
+   * @param line line to split
+   * @return an array of strings by splitting the input around matches of this delimiter
+   */
+  public String[] split(String line) {
+    return regexValue()
+            .map(line::split)
+            .orElse(new String[]{line});
+  }
+
+  @Override
+  public String[] apply(String line) {
+    return split(line);
+  }
+}
